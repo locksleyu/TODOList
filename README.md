e69757fdbd94fb19745a084e239e4335efc5c9c1
jeffduet <jeff@duetdisplay.com>
2024-10-01 15:54:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TODOList
+ TODO List app
---
+++ TODOList/TODOListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOListApp.swift
+//  TODOList
+//
+//  Created by Jeffrey Wisgo on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TODOListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
