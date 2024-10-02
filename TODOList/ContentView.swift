ee793b4f2c66fe7453cf8779b2d6b583583d8567
jeffduet <jeff@duetdisplay.com>
2024-10-01 17:22:17 -0700

    add basic logic to pull from URL

--- TODOList/ContentView.swift
+++ TODOList/ContentView.swift
@@ -7,24 +7,70 @@
 
 import SwiftUI
 
+// https://jsonplaceholder.typicode.com/todos
+/*
+"userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+*/
+
+struct TodoItem: Codable, Identifiable {
+	let userId: Int
+	let id: Int
+	let title: String
+	let completed: Bool
+}
+
 struct ContentView: View {
+	@State private var todoItems: [TodoItem] = []
+	private func fetchRemoteData() {
+			let url = URL(string: "https://jsonplaceholder.typicode.com/todos?userId=3")!
+			var request = URLRequest(url: url)
+			request.httpMethod = "GET"  // optional
+			request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+			let task = URLSession.shared.dataTask(with: request){ data, response, error in
+				if let error = error {
+					print("Error while fetching data:", error)
+					return
+				}
+
+				guard let data = data else {
+					return
+				}
+
+				do {
+					let decodedData = try JSONDecoder().decode([TodoItem].self, from: data)
+					// Assigning the data to the array
+					self.todoItems = decodedData
+				} catch let jsonError {
+					print("Failed to decode json", jsonError)
+				}
+			}
+
+			task.resume()
+		}
 	var body: some View {
 		VStack {
 			Text("TODO List").font(.headline)
-			List {
+			List (todoItems) { item in
 				Label {
-					Text("Task 1")
+					Text(item.title)
 				} icon: {
-					Label("", systemImage: "checkmark")			.foregroundStyle(.gray)
+					Label("", systemImage: "checkmark")
+						.foregroundStyle(.gray)
 				}
 			}
 			.listStyle(InsetGroupedListStyle())
+			.onAppear {
+				fetchRemoteData()
+			}
 		}
 	}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+		ContentView()
     }
 }
