450d439cb22837f3e965c0d32f41efebce7624c1
jeffwisgo <jeff@duetdisplay.com>
2024-10-04 09:17:05 -0700

    equality and special logic tests

--- TODOListTests/TODOListTests.swift
+++ TODOListTests/TODOListTests.swift
@@ -40,5 +40,35 @@ final class TODOListTests: XCTestCase {
 		
 		wait(for: [expectation], timeout: 30.0)
 	}
-	
+	func testItemEquality() throws {
+		let item1 = TodoItem(userId: 1, id: 1, title: "item 1", completed: false)
+		let item2 = TodoItem(userId: 1, id: 1, title: "item 1", completed: false)
+		let item3 = TodoItem(userId: 1, id: 2, title: "item 3", completed: false)
+		let item4 = TodoItem(userId: 2, id: 1, title: "item 1", completed: false)
+		let item5 = TodoItem(userId: 1, id: 2, title: "item 1", completed: false)
+		let item6 = TodoItem(userId: 1, id: 1, title: "item 1", completed: true)
+		
+		XCTAssert(item1 == item2)
+		XCTAssert(item1 != item3)
+		XCTAssert(item1 != item4)
+		XCTAssert(item1 != item5)
+		XCTAssert(item1 != item6)
+	}
+	func testSpecialItemLogic() throws {
+		let item1 = TodoItem(userId: 1, id: 1, title: "item 1", completed: false)
+		let item2 = TodoItem.createAddItem()
+		
+		XCTAssert(item1.isRegularItem())
+		XCTAssertFalse(item1.isAddItem())
+		
+		XCTAssert(item2.isAddItem())
+		XCTAssertFalse(item2.isRegularItem())
+		
+		// sanity check to make sure values were properly set
+		
+		XCTAssert(item1.userId == 1)
+		XCTAssert(item1.id == 1)
+		XCTAssert(item1.title == "item 1")
+		XCTAssert(item1.completed == false)
+	}
 }
